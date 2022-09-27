96e9a2c66ec939554d1e0126ac7ed0cc9be6b90a
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-09-26 22:46:41 -0300

    tabnav transformado em class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
+import TabNav from './modules/tabnav.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -8,7 +9,6 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
-import Accordion from './modules/accordion.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -16,8 +16,10 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
 initAnimacaoScroll();
-initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
