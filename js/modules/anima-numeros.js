e29017736d54041e09a3601db518c29474038aba
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-10-01 11:24:15 -0300

    Refatorado fetch-animais

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -8,7 +8,7 @@ export default class AnimaNumeros {
     this.handleMutation = this.handleMutation.bind(this);
   }
 
-  // Recebe um elemento do dom, com número em seu texto, incremente a partir de 0 até o número final
+  // [5] - Recebe um elemento do dom, com número em seu texto, incremente a partir de 0 até o número final
   static incrementarNumeros(numero) {
     const total = +numero.innerText;
     const incremento = Math.floor(total / 100);
@@ -23,12 +23,12 @@ export default class AnimaNumeros {
     }, 25 * Math.random());
   }
 
-  // Ativa incrementar número para cada número selecionado do dom
+  // [4] - Ativa incrementar número para cada número selecionado do dom
   animaNumeros() {
     this.numeros.forEach(numero => this.constructor.incrementarNumeros(numero));
   }
 
-  // Função que corre quadndo a mutação ocorrer
+  // [3] - Função que corre quadndo a mutação ocorrer
   handleMutation(mutation) {
     if (mutation[0].target.classList.contains(this.observerClass)) {
       this.observer.disconnect();
@@ -36,16 +36,18 @@ export default class AnimaNumeros {
     }
   }
 
-  // Adiciona o MutationObserver para verificar quando a classe ativo é adicionada ao elemento target
+  // [2] - Adiciona o MutationObserver para verificar quando a classe ativo é adicionada ao elemento target
   addMutationObserver() {
     this.observer = new MutationObserver(this.handleMutation);
     this.observer.observe(this.observerTarget, { attributes: true });
   }
 
+  // [1]
   init() {
     if (this.numeros.length && this.observerTarget) {
       this.addMutationObserver();
     }
     return this;
   }
+
 }
