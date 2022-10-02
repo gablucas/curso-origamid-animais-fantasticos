06922e872c06c3c335104633a052bcd5a1eb54ee
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-10-02 18:44:42 -0300

    fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import fetchAnimais from './modules/fetch-animais.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
+
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -27,8 +28,10 @@ tooltip.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
 initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
+
