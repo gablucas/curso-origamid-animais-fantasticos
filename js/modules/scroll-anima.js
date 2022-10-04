1c675978e239a68fcb7b428c78eba2f7c24a05b6
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-10-03 22:35:40 -0300

    Animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,22 +2,46 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    
-    this.animaScroll = this.animaScroll.bind(this);
+
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      }
+  // Pega a distância de cada item em relação ao topo site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.windowMetade,
+      };
     });
   }
 
+  // Verifica a distância em cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((section) => {
+      if (window.scrollY > section.offset) {
+        section.element.classList.add('ativo');
+      }
+    });
+
+    if (window.scrollY > this.distance[this.distance.length - 1].offset) {
+      this.stop();
+    }
+  }
+
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o event de Scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
