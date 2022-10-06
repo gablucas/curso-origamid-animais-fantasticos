e056f0dd04ee847041dc0c0ca814786cfda23e66
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-10-05 21:42:20 -0300

    Adicionado debounce na animacao ao scroll

---
+++ js/modules/debounce.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/prefer-default-export */
+export function debounce(callback, delay = 1000) {
+  let timer;
+
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      callback(...args);
+      timer = null;
+    }, delay);
+  };
+}
